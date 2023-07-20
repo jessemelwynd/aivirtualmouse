07bf00b564a350d5e2af0fe79a29070a0e97bd93
jessemelwynd <113762115+jessemelwynd@users.noreply.github.com>
2023-07-20 19:32:34 +0530

    Update handtrackingmodule.py

--- handtrackingmodule.py
+++ handtrackingmodule.py
@@ -3,6 +3,8 @@ import mediapipe as mp
 import time
 import math
 import numpy as np
+
+
 class handDetector():
     def __init__(self, mode=False, maxHands=2, detectionCon=0.5, trackCon=0.5):
         self.mode = mode
